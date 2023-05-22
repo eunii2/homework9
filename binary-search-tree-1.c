f67fbe87ca2d5d866ecdad6f7bbce1fdd6a658d9
GaeunChoi <chb7562@gmail.com>
2023-05-22 14:27:45 +0900

    second commit

--- binary-search-tree-1.c
+++ binary-search-tree-1.c
@@ -28,12 +28,14 @@ int deleteLeafNode(Node* head, int key);  /* delete the leaf node for the key */
 Node* searchRecursive(Node* ptr, int key);  /* search the node for the key */
 Node* searchIterative(Node* head, int key);  /* search the node for the key */
 int freeBST(Node* head); /* free all memories allocated to the tree */
-
+void freeNode(Node* ptr);
 /* you may add your own defined functions if necessary */
 
 
 int main()
 {
+	printf("[-----[Choi Gaeun][2022078054]-----]");
+
 	char command;
 	int key;
 	Node* head = NULL;
@@ -111,59 +113,172 @@ int main()
 
 int initializeBST(Node** h) {
 
-	/* if the tree is not empty, then remove all allocated nodes from the tree*/
 	if(*h != NULL)
 		freeBST(*h);
 
-	/* create a head node */
 	*h = (Node*)malloc(sizeof(Node));
-	(*h)->left = NULL;	/* root */
+	(*h)->left = NULL;
 	(*h)->right = *h;
 	(*h)->key = -9999;
 	return 1;
 }
 
-
-
 void inorderTraversal(Node* ptr)
 {
-
+	if(ptr) {
+		inorderTraversal(ptr->left);
+		printf(" [%d] ", ptr->key);
+		inorderTraversal(ptr->right);
+	}
 }
 
 void preorderTraversal(Node* ptr)
 {
-
+	if(ptr) {
+		printf(" [%d] ", ptr->key);
+		preorderTraversal(ptr->left);
+		preorderTraversal(ptr->right);
+	}
 }
 
 void postorderTraversal(Node* ptr)
 {
-
+	if(ptr) {
+		postorderTraversal(ptr->left);
+		postorderTraversal(ptr->right);
+		printf(" [%d] ", ptr->key);
+	}
 }
-
 
 int insert(Node* head, int key)
 {
+	Node* newNode = (Node*)malloc(sizeof(Node));
+	newNode->key = key;
+	newNode->left = NULL;
+	newNode->right = NULL;
 
+	if (head->left == NULL) {
+		head->left = newNode;
+		return 1;
+	}
+	
+	Node* ptr = head->left;
+	Node* parentNode = NULL;
+
+	while(ptr != NULL) {
+		if(ptr->key == key) return 1;
+
+		parentNode = ptr;
+
+		if(ptr->key < key)
+			ptr = ptr->right;
+		else
+			ptr = ptr->left;
+	}
+
+	if(parentNode->key > key)
+		parentNode->left = newNode;
+	else
+		parentNode->right = newNode;
+	return 1;
 }
 
 int deleteLeafNode(Node* head, int key)
 {
+	if (head == NULL) {
+		printf("\n Nothing to delete!!\n");
+		return -1;
+	}
 
+	if (head->left == NULL) {
+		printf("\n Nothing to delete!!\n");
+		return -1;
+	}
+
+	Node* ptr = head->left;
+	Node* parentNode = head;
+
+	while(ptr != NULL) {
+
+		if(ptr->key == key) {
+			if(ptr->left == NULL && ptr->right == NULL) {
+				if(parentNode == head)
+					head->left = NULL;
+				if(parentNode->left == ptr)
+					parentNode->left = NULL;
+				else
+					parentNode->right = NULL;
+				free(ptr);
+			}
+			else {
+				printf("the node [%d] is not a leaf \n", ptr->key);
+			}
+			return 1;
+		}
+
+		parentNode = ptr;
+
+		if(ptr->key < key)
+			ptr = ptr->right;
+		else
+			ptr = ptr->left;
+	}
+
+	printf("Cannot find the node for key [%d]\n ", key);
+
+	return 1;
 }
 
 Node* searchRecursive(Node* ptr, int key)
 {
+	if(ptr == NULL)
+		return NULL;
+
+	if(ptr->key < key)
+		ptr = searchRecursive(ptr->right, key);
+	else if(ptr->key > key)
+		ptr = searchRecursive(ptr->left, key);
+
+	return ptr;
 
 }
-
 Node* searchIterative(Node* head, int key)
 {
+	Node* ptr = head->left;
 
+	while(ptr != NULL)
+	{
+		if(ptr->key == key)
+			return ptr;
+
+		if(ptr->key < key) ptr = ptr->right;
+		else
+			ptr = ptr->left;
+	}
+
+	return NULL;
 }
-
 
 int freeBST(Node* head)
 {
 
+	if(head->left == head){
+		free(head);
+		return 1;
+	}
+	Node* p = head->left;
+
+	freeNode(p);
+
+	free(head);
+	return 1;
 }
 
+void freeNode(Node* ptr)
+{
+	if(ptr) {
+		freeNode(ptr->left);
+		freeNode(ptr->right);
+		free(ptr);
+	}
+}
